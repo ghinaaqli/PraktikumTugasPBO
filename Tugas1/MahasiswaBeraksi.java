6908566bfd905aadbf64e70514061ff4d153c0c6
Ghina Aqli Syahirah <114642992+ghinaaqli@users.noreply.github.com>
2023-10-11 22:05:15 +0700

    Tugas 1 PBO ghina aqli

---
+++ Tugas1/MahasiswaBeraksi.java
@@ -0,0 +1,13 @@
+class MahasiswaBeraksi {
+  void membaca() {
+    System.out.println("Saya sedang membaca buku.");
+  }
+
+  void nyontek() {
+    System.out.println("Saya tidak boleh nyontek, itu melanggar etika belajar.");
+  }
+
+  void modifikasi() {
+    System.out.println("Saya sedang memodifikasi kode program.");
+  }
+}
